a4619fb0305a896cd6857410d98881c9057fbe79
Matthew Seymour <matthew.seymour@gmail.com>
2016-02-28 12:45:24 -0500

    Delete dead code

--- shader-include-packData.js
+++ shader-include-packData.js
@@ -31,23 +31,5 @@ vec4 pack(float value) { //Range [-1, 1.00003...]
 	return vec4(bytes.x, bytes.y, 0.0, 0.0);
 }
 
-/*
-vec2 unpack(vec4 value) {
-	vec2 bytes = vec2(unpack2Bytes(value.xz), unpack2Bytes(value.yw)); //Range [0, 65535]
-	
-	vec2 signed = bytes - 32767.0; //Range [-32767, 32768]
-	
-	return signed / 32767.0; //Range [-1, 1.00003...]
-}
 
-vec4 pack(vec2 value) { //Range [-1, 1.00003...] 
-	vec2 signed = value * 32767.0; //Range[-32767, 32768]
-	vec2 bytes = signed + 32767.0;  //Range[0, 65535]
-	
-	vec2 bytesA = pack2Bytes(bytes.x); //Range [(0,0), (1,1)]
-	vec2 bytesB = pack2Bytes(bytes.y); //Range [(0,0), (1,1)]
-	
-	return vec4(bytesA.x, bytesB.x, bytesA.y, bytesB.y);
-}
-*/
 `;
